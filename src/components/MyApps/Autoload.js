fccd4c8327a2630d47919520f5913029fceecc5c
mistik9 <mistik9@icloud.com>
2024-03-12 16:05:00 +0600

    change autoload desc

--- src/components/MyApps/Autoload.js
+++ src/components/MyApps/Autoload.js
@@ -12,7 +12,7 @@ function Autoload({ handleClick, isDesc }) {
             <div className="my-app__title-container">
                 <img className="my-app__img" src="//util.1c-bitrix.ru/upload/bx24vendor/892/h01fm2n4a26e3n8eam3h15h8ckldh4n1/Autoload.png?1709203801464" alt="иконка"></img>
                 <h3 className="my-app__title">Avito: автозагрузка</h3>
-                <p className="my-app__text">Позволяет опубликовывать и обновлять список ваших товаров (услуг) на Avito во всех категориях.</p>
+                <p className="my-app__text">Выгружайте Ваши объявления на Avito из Битрикс24 автоматически.</p>
                 <a href="https://www.bitrix24.ru/apps/app/integrationpart.avito_avtozagruzka/" target="_blank" className="my-app__link">Ссылка на приложение </a>
             </div>
             <div className="my-app__menu">
